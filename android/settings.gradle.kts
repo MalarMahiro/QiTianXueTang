fde71510d4ac8ab35d1d889ec2a47b5b761df6fa
xiaoniao427@163.com <xiaoniao427@163.com>
2026-08-27 20:51:50 +0800

    只留官方默认源

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -11,11 +11,6 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/public")
-        maven("https://repo.huaweicloud.com/repository/gradle-plugin/")
-        maven("https://repo.huaweicloud.com/repository/maven/")
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
